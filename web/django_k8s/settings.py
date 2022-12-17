c64cd42c56ef8020d19c1b0e9cf51920c3ffd88e
mike-jianxin-mo <mike.mo.jianxin@gmail.com>
2022-12-17 21:54:26 +1100

    feat: add env variables to docker composor

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY") 
+SECRET_KEY = os.environ.get("SECURITY_KEY") 
 # 'django-insecure-vx6xa3g_vufe7!io@y5o6njz^$ajbs=--_e2s%@uu!)=8b$z16'
 
 # SECURITY WARNING: don't run with debug turned on in production!
