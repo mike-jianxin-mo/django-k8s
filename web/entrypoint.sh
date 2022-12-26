7e1b9174ba436190f6e6ecb3568db84e34897acb
mike-jianxin-mo <mike.mo.jianxin@gmail.com>
2022-12-26 21:29:56 +1100

    feat: add script for debugging errors directly with Django.

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 APP_PORT=${PORT:-8000}
 cd /app/
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm django_k8s.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+/opt/venv/bin/gunicorn --timeout 300 --worker-tmp-dir /dev/shm django_k8s.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+# /opt/venv/bin/python manage.py runserver 0.0.0.0:8020
