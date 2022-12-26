bddd79a5a896321f5a263368d8ee15922ad9e03b
mike-jianxin-mo <mike.mo.jianxin@gmail.com>
2022-12-26 21:29:16 +1100

    fix: fix an error in migration script.

--- web/migrate.sh
+++ web/migrate.sh
@@ -2,7 +2,7 @@
 
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"hello@qq.com"}
 
-cd /app/
+cd /app
 
-/opt/venv/bin/python manage.py --noinput
+/opt/venv/bin/python manage.py makemigrations --noinput
 /opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
